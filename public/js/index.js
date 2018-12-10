1abb08375172d59702f8ed2fc2ecfb4dc6ebe29d
Emilly <dawid.pietras.91@gmail.com>
2018-12-10 17:43:29 +0100

    emit

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from:'Dave',
-    text: 'That works'
-  });
 });
 
 socket.on('disconnect', function () {
